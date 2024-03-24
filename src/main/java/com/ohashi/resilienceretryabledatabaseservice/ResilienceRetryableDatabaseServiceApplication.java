e8ad09fa6eecb9c169e2a75c2c811f4f4618ff57
ImOhashi <leonardoohashi.mkt@gmail.com>
2024-03-24 18:03:28 -0300

    first commit

---
+++ src/main/java/com/ohashi/resilienceretryabledatabaseservice/ResilienceRetryableDatabaseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ohashi.resilienceretryabledatabaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResilienceRetryableDatabaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResilienceRetryableDatabaseServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohashi/resilienceretryabledatabaseservice/ResilienceRetryableDatabaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohashi.resilienceretryabledatabaseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResilienceRetryableDatabaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
